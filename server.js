b6aebf4bb4ec04637dec41efda707e19076bb4a1
Nebojša Jarić <jaricn@gmail.com>
2020-06-26 11:47:55 +0200

    implement API calls

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
-//const bodyParser = require('body-parser')
-const path = require('path');
-const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser')
+const axios = require('axios');
 const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
 const root = 'https://5ebd9842ec34e900161923e7.mockapi.io/post';
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
-    return res.send('pong');
-});
+app.use(cors({
+    methods: ['GET']
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/post', function (req, res) {
-    return res.send(root);
+    axios.get(root).then(function (response) {
+        res.send(response.data);
+    });
 });
 
 app.get('/comments/:id', function (req, res) {
     const url = `${root}/${req.params.id}/comments`;
-    return res.send(url);
+
+    axios.get(url).then(function (response) {
+        res.send(response.data);
+    });
 });
 
-app.listen(port);
+app.listen(port, () => console.log(`Listening on port ${port}`));
