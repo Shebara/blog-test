30ea17100a652dec3046f03cccdb6b2b7991f286
Nebojša Jarić <jaricn@gmail.com>
2020-06-27 14:47:37 +0200

    rename ListPost test

--- src/tests/ListPost.test.js
+++ src/tests/ListPost.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
 import ListPosts from '../components/ListPosts';
+import store from '../store'
 
 describe('ListPosts', () => {
     it('should render without errors', () => {
-        const component = shallow(<ListPosts />);
+        const component = shallow(
+            <Provider store={store}>
+                <ListPosts />
+            </Provider>
+        );
 
         expect(component.getElements()).toMatchSnapshot();
     });
